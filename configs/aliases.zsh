d9b253a28a76d94e83af3034f4c79506d59c4ae8
Arjun Makwana <arjunmatwork@gmail.com>
2026-02-02 22:46:51 +0000

    Added new alias and changed theme

--- configs/aliases.zsh
+++ configs/aliases.zsh
@@ -4,6 +4,7 @@
 
 # Your original personal aliases
 alias ltr='ls -ltr'
+alias altr='ls -altr'
 alias sshrd='ssh dev-dsk-arjunmkw-2b-4998fc23.us-west-2.amazon.com'
 
 # Load local aliases if they exist (machine-specific)
